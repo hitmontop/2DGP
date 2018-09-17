8dbacbd366bd665aaaae3414061bc13ba98ee656
hitmontop(notebook) <pinkjery@naver.com>
2018-09-17 20:53:41 +0900

    점1에서 점2 으로 가는 함수 완성
    점1에서 점2로 가는 함수이다.
    점과 점 사이의 거리를 10등분 하여 10번에 나누어 가도록 했다.
    방향에 따라 왼쪽 또는 오른쪽으로 간다

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -1,14 +1,37 @@
 from pico2d import *
-
 open_canvas()
 
-grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
 def move_to_1():
     pass
+
 def move_to_2():
-    pass
+    x, y = 203, 535
+    x2, y2 = 132, 243
+
+    frame = 0
+    dir = x2-x
+    # s = (x2-x) / (y2-y)
+    # d = ((((x2-x)**2) / ((y2-y)**2))**0.5)/10
+    dx = (x2-x)/10.0
+    dy = (y2-y)/10.0
+
+    while x > 132:
+        clear_canvas_now()
+        if dir >= 0:
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        else:
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += dx
+        y += dy
+
+
+        delay(0.01)
+
 def move_to_3():
     pass
 def move_to_4():
