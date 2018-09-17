5fab3b62ad9af6224e390fa0adaba4c4d7f0de80
hitmontop(notebook) <pinkjery@naver.com>
2018-09-17 21:12:34 +0900

    고개를 돌리는 방향 결정
    두 점의 x좌표끼리 비교해서 어느쪽을 볼건지 걸졍하게 만들었다.

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -6,28 +6,45 @@ character = load_image('animation_sheet.png')
 def move_p1_to_p2(x, y, x2, y2):
 
     frame = 0
-    dir = x2-x
+    direc = x2-x
     # s = (x2-x) / (y2-y)
     # d = ((((x2-x)**2) / ((y2-y)**2))**0.5)/10
     dx = (x2-x)/10.0
     dy = (y2-y)/10.0
 
-    while x > 132:
-        clear_canvas_now()
-        if dir >= 0:
-            character.clip_draw(frame * 100, 100, 100, 100, x, y)
-        else:
-            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    if direc <= 0:
+        while x > x2:
+            clear_canvas_now()
+            character.clip_draw(frame * 100, 0 , 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x += dx
+            y += dy
+            delay(0.05)
 
-        update_canvas()
-        frame = (frame + 1) % 8
-        x += dx
-        y += dy
-        delay(0.01)
+    else:
+        while x < x2:
+            clear_canvas_now()
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x += dx
+            y += dy
+            delay(0.05)
 
 
 def make_character_move():
     move_p1_to_p2(203, 535, 132, 243)
+    move_p1_to_p2(132, 243, 535, 470)
+    # move_p1_to_p2(535, 470, 477, 203)
+    # move_p1_to_p2(477, 203, 715, 136)
+    # move_p1_to_p2(715, 136, 316, 225)
+    # move_p1_to_p2(316, 225, 510, 92)
+    # move_p1_to_p2(510, 92, 692, 518)
+    # move_p1_to_p2(692, 518, 682, 336)
+    # move_p1_to_p2(682, 336, 712, 349)
+    # move_p1_to_p2(712, 349, 203, 535)
+
 
 while True:
     make_character_move()
