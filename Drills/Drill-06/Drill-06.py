d5621e3f926f946f0567b1ce043518921c6f2465
hitmontop(notebook) <pinkjery@naver.com>
2018-09-20 17:03:55 +0900

    좌표 여러개 설정
    계산에 필요한좌표와 이벤트함수 설정

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -6,6 +6,10 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global sx, sy
+    global newx, newy
+    global is_running
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -14,22 +18,31 @@ def handle_events():
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running == False
+        elif SDL_MOUSEBUTTONDOWN:
+            if is_running == False:
+                x1, y1 = x2, y2
+                x2, y2= x,y
+                is_running = True
 
 
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-hand = load_image('hand_arrow')
+hand = load_image('hand_arrow.png')
 
 running = True
+is_running = False
+
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x2, y2 = KPU_WIDTH // 2, KPU_HEIGHT // 2
+
 frame = 0
-hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(x, y)
     update_canvas()
     frame = (frame + 1) % 8
 
