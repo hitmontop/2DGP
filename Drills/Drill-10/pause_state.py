b59755632ff63f89099c9cd109ac875a846b6d1c
hitmontop(notebook) <pinkjery@naver.com>
2018-10-11 21:08:41 +0900

    drill10-1 시작
    pause 는 되는데 main의 저장이 동작하지 않음

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,58 @@
+import game_framework
+from pico2d import*
+import main_state
+
+name = "PauseState"
+pause_button = None
+
+class PauseButton:
+    def __init__(self):
+        self.image = load_image('pause.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+    def update(self):
+        pass
+
+
+def enter():
+    global pause_button
+    pause_button = PauseButton()
+
+
+
+def exit():
+    global pause_button
+    del(pause_button)
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+                game_framework.change_state(main_state)
+
+
+def draw():
+    clear_canvas()
+    pause_button.draw()
+    update_canvas()
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
